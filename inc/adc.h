cdf21d14c992a199fc241770a455f736d1449420
260245 <80456316+VinayakKeshavKatigar@users.noreply.github.com>
2021-04-30 00:26:02 +0530

    Add files via upload

---
+++ inc/adc.h
@@ -0,0 +1,24 @@
+
+
+/**
+ * @file adc.h
+ * @author vinayak katigar (vvkatigar1999@gmail.com)
+ * @brief ADC function definition
+ * @version 0.1
+ * @date 2021-04-26
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef __ADC_H__
+#define __ADC_H__
+/**
+ * @brief Reading the analog input and returning a digital 8bit value
+ *
+ * @param ch
+ * @return uint16_t
+ */
+uint16_t adc(uint8_t ch);
+void InitADC();
+#endif
---
+++ inc/pwm.h
@@ -0,0 +1,22 @@
+
+/**
+ * @file pwm.h
+ * @author vinayak katigar (vvkatigar1999@gmail.com)
+ * @brief Display the output of the temperature sensor
+ * @version 0.1
+ * @date 2021-04-29
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#ifndef __PWM_H__
+#define __PWM_H__
+#include<avr/io.h>
+/**
+ * @brief Function definition
+ *
+ * @param temp_value
+ */
+void pwm(uint16_t temp_value);
+void InitPWM(void);
+#endif
