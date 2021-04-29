945f610c440873463ab9ccac3e816ea22142e6c4
260245 <80456316+VinayakKeshavKatigar@users.noreply.github.com>
2021-04-30 00:28:53 +0530

    Add files via upload

---
+++ src/led.c
@@ -0,0 +1,32 @@
+
+
+/**
+ * @file led.c
+ * @author vinayak katigar (vvkatigar19992gmail.com)
+ * @brief led state Changes with two switch
+ * @version 0.1
+ * @date 2021-04-23
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+// include files
+#include<avr/io.h>
+#include "led.h"
+/**
+ * @brief Change the state of the LED Pin according to the value of state
+ *
+ * @param state Pin level to which the LED Pin should be set
+ */
+void led(uint8_t state)
+{
+	LED_PORT = (state << LED_PIN); //States LED pin is ON or OFF
+}
+void InitLED(void)
+{
+	DDRB|=(1<<PB0);
+    DDRD&=~(1<<PD0);
+    PORTD|=(1<<PD0);
+    DDRD&=~(1<<PD1);
+    PORTD|=(1<<PD1);
+}
---
+++ src/pwm.c
@@ -0,0 +1,113 @@
+
+
+/**
+ * @file pwm.c
+ * @author vinayak katigar (vvkatigar1999@gmail.com)
+ * @brief PWM wave according to temperature
+ * @version 0.1
+ * @date 2021-04-27
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#include"pwm.h"
+#include<util/delay.h>
+#include<avr/io.h>
+/**
+ * @brief initialize timer and counter register
+ *
+ */
+void InitPWM(void)
+{
+    /*Configuring the registers and ports*/
+    TCCR1A|=(1<<COM1A1)|(1<<WGM10)|(1<<WGM11);
+    TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10);
+    DDRB|=(1<<PB1);
+
+}
+/**
+ * @brief Analog input coverted in digital ADC is used optain that
+ *
+ * @param ticks
+ */
+void pwm (uint16_t ticks)
+{
+
+
+    if((ticks>=0) && (ticks<=103))
+        {
+
+        OCR1A = 103; //10% duty cycle
+
+        _delay_ms(20);
+    }
+    else if((ticks>=104) && (ticks<=205))
+        {
+
+        OCR1A = 205; //20% duty cycle
+
+       _delay_ms(20);
+    }
+    else if((ticks>=206) && (ticks<=308))
+        {
+
+        OCR1A = 308;//30% duty cycle
+
+        _delay_ms(20);
+    }
+    else if((ticks>=309) && (ticks<=410))
+        {
+
+        OCR1A = 410; //40% duty cycle
+
+        _delay_ms(20);
+    }
+    else if((ticks>=411) && (ticks<=512))
+        {
+
+        OCR1A = 512; //50% duty cycle
+
+        _delay_ms(20);
+    }
+    else if((ticks>=513) && (ticks<=615))
+        {
+
+        OCR1A = 615; //60% duty cycle
+
+        _delay_ms(20);
+    }
+    else if((ticks>=616) && (ticks<=717))
+        {
+
+        OCR1A = 717; //70% duty cycle
+
+        _delay_ms(20);
+    }
+    else if((ticks>=718) && (ticks<=820))
+        {
+
+        OCR1A = 820; //80% duty cycle
+
+        _delay_ms(20);
+    }
+    else if((ticks>=821) && (ticks<=922))
+        {
+
+        OCR1A = 922; //90% duty cycle
+
+        _delay_ms(20);
+    }
+    else if((ticks>=923) && (ticks<=1024))
+        {
+
+        OCR1A = 1024; //100% duty cycle
+
+        _delay_ms(20);
+    }
+    else
+    {
+        OCR1A = 0; //0% output
+
+    }
+}
+
