0f66831dbcd25e8ef316ca464906f5c2b3130b8a
260245 <80456316+VinayakKeshavKatigar@users.noreply.github.com>
2021-04-30 21:17:08 +0530

    Update uart.h

--- inc/uart.h
+++ inc/uart.h
@@ -12,12 +12,15 @@
 #ifndef __UART_H__
 #define __UART_H__
 #include<avr/io.h>
+
 /**
  * @brief Inittialize UART serial communication ports
  *
  * @param ubrr
  */
+
 void InitUART(uint16_t ubrr);
+
 /**
  * @brief Read characters from UART port
  * @return char
