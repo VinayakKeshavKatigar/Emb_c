2abebb2a29f65f8814b3004e8d995b0b3ecc5250
260245 <80456316+VinayakKeshavKatigar@users.noreply.github.com>
2021-04-30 21:16:37 +0530

    Update pwm.h

--- inc/pwm.h
+++ inc/pwm.h
@@ -1,4 +1,3 @@
-
 /**
  * @file pwm.h
  * @author vinayak katigar (vvkatigar1999@gmail.com)
@@ -9,14 +8,17 @@
  * @copyright Copyright (c) 2021
  *
  */
+
 #ifndef __PWM_H__
 #define __PWM_H__
 #include<avr/io.h>
+
 /**
  * @brief Function definition
  *
  * @param temp_value
  */
+
 void pwm(uint16_t temp_value);
 void InitPWM(void);
 #endif
